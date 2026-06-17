cdfcd2d6bc81e3fb9cb78cae661b6c15e7c1ea61
TennWiciuu <wprzygodzki17@gmail.com>
2026-06-17 12:46:51 +0200

    Poprawienie UX (kliknięcie poza opcje zamyka je)

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    document.querySelectorAll('.options-btn').forEach(btn => {
-        btn.addEventListener('click', function () {
+    const buttons = document.querySelectorAll('.options-btn');
+
+    buttons.forEach(btn => {
+        btn.addEventListener('click', function (e) {
+            e.stopPropagation();
 
             const dropdown = this.nextElementSibling;
             dropdown.classList.toggle('show');
 
+            document.querySelectorAll('.dropdown').forEach(d => {
+                if (d !== dropdown) {
+                    d.classList.remove('show');
+                }
+            });
+        });
+    });
+
+    document.addEventListener('click', function () {
+        document.querySelectorAll('.dropdown').forEach(d => {
+            d.classList.remove('show');
         });
     });
 
